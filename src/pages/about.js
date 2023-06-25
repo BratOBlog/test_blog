5426c174e02f431ea2f58b521661344e16fedb15
Tibor <bratoblog@gmail.com>
2023-06-25 07:05:35 +0200

    add files

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,16 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <main  className="bg-lime-500">
+      <h1 className="text-3xl font-bold">About Me</h1>
+      <Link to="/" className="text-blue-500">Back to Home</Link>
+      <p className="mt-4">Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
 }
 
-
+export const Head = () => <title>About Me</title>
 
 export default AboutPage
