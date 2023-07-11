ccaf6d404b6b7a68677b9ee5922b6f1190d2f74a
Tibor <bratoblog@gmail.com>
2023-07-11 14:51:49 +0200

    change the quote file

--- src/pages/blog/quote.js
+++ src/pages/blog/quote.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
 const MyQuote = () => {
@@ -15,26 +15,33 @@ const MyQuote = () => {
   `);
 
   const quotes = data.customApi.quotes;
-
   const [randomQuote, setRandomQuote] = useState(null);
 
-  const generateRandomQuote = () => {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const quote = quotes[randomIndex];
-  setRandomQuote(quote);
-  };
+  useEffect(() => {
+    const generateRandomQuote = () => {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const quote = quotes[randomIndex];
+      setRandomQuote(quote);
+    };
 
-  useState(() => {
     generateRandomQuote();
-  }, []);
+
+    const interval = setInterval(() => {
+      generateRandomQuote();
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [quotes]);
 
   return (
     <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md p-4">
-    {randomQuote && (
-      <div key={randomQuote.quote}>
-        <p className="italic">{randomQuote.quote}</p>
-        <p className="text-right">- {randomQuote.author}</p>
-      </div>
+      {randomQuote && (
+        <div key={randomQuote.quote}>
+          <p className="italic">{randomQuote.quote}</p>
+          <p className="text-right">- {randomQuote.author}</p>
+        </div>
       )}
     </div>
   );
